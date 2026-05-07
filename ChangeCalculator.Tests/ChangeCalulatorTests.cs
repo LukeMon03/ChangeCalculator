872c19cc44ca8cb9b3cd6a36e4aba603a57e9cda
Luke Monaghan <LukeMon03>
2026-05-07 21:00:58 +0100

    Corrected spelling mistake, removed duplicate solution, added unit test project, updated gitIgnore

---
+++ ChangeCalculator.Tests/ChangeCalulatorTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using ChangeCalculator;
+
+namespace ChangeCalculator.Tests
+{
+    public class ChangeCalulatorTests
+    {
+        private readonly ChangeCalculator _calculator = new();
+
+        [Fact]
+        public void ChangeCalulator_ReturnsCorrectAmountOfChangeWhenGivenValidInput()
+        {
+
+        }
+    }
+}
